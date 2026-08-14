7141e42af933bf2a99972bd646e8b59d0d21e251
Topaz <graffitihooves@gmail.com>
2026-08-14 00:05:06 -0400

    Save local changes before merge

--- PaleoPinesDinoStudio/obj/Debug/PaleoPinesDinoStudio.AssemblyInfo.cs
+++ PaleoPinesDinoStudio/obj/Debug/PaleoPinesDinoStudio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaleoPinesDinoStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e318af60b0ddf1ce042bd091c7ac31304e7897f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaleoPinesDinoStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaleoPinesDinoStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
