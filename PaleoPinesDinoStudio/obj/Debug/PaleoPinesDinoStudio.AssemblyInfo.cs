a6c361e68faf61cc84acda98450e95b7c1f4629e
Topaz <graffitihooves@gmail.com>
2026-09-20 21:32:01 -0400

    Simplified pattern names, added full dino names, fixed missing protoceratops info

--- PaleoPinesDinoStudio/obj/Debug/PaleoPinesDinoStudio.AssemblyInfo.cs
+++ PaleoPinesDinoStudio/obj/Debug/PaleoPinesDinoStudio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaleoPinesDinoStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69f1c98ae1560ce6d38fd17a8bf6742b33db8328")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7181586eaed734a3e80477f03ca03087da57dcf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaleoPinesDinoStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaleoPinesDinoStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
